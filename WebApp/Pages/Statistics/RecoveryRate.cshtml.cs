fbc1bcfb19be3ba8dcea98554bf0139839905395
Kiko Bela <kiko_bela_mkd@hotmail.com>
2020-03-28 16:49:19 +0100

    update5

--- WebApp/Pages/Statistics/RecoveryRate.cshtml.cs
+++ WebApp/Pages/Statistics/RecoveryRate.cshtml.cs
@@ -36,8 +36,8 @@ namespace WebApp.Pages.Statistics
                 RecoveryRate.Add(new StatisticsCore
                 {
                     Recovered = item.Key==true ?
-                    "Recovered " + String.Format("{0:0.00}", ((double)item.Count() / Diagnoses.Count()) * 100) + "%" 
-                    : "Deaths " + String.Format("{0:0.00}", ((double)item.Count()/Diagnoses.Count())*100) + "%",
+                    "Recovered " + String.Format("{0:0.00}", ((double)item.Count() / (Deaths.Count() + Recovered.Count())) * 100) + "%" 
+                    : "Deaths " + String.Format("{0:0.00}", ((double)item.Count()/ (Deaths.Count() + Recovered.Count())) *100) + "%",
                     TotalPatients = item.Count()
                 });
             }
